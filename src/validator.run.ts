37505f5ea0e82d165dee8dfdc37a22f6fd7c46aa
Wojciech Marek <trustme@onet.pl>
2022-10-23 21:49:13 +0200

    creating server

---
+++ src/validator.run.ts
@@ -0,0 +1,19 @@
+import Express from "express";
+import { Server } from "../config";
+import helmet from "helmet";
+import cors from "cors"
+import routes from "./routes-tree/routes-index"
+
+const app = Express()
+
+app.use(cors());
+app.use(helmet());
+app.use(Express.json());
+app.use("/", routes);
+app.get("/",(req, res)=>{
+        res.send('VALIDATOR')
+})
+
+app.listen(Server.Port, ()=>{
+        console.log(`Validator created: ${Server.Host}:${Server.Port}`);
+})
