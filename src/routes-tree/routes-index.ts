d62040d73771730655f5a5627b4c1fdf30b7739b
Wojciech Marek <trustme@onet.pl>
2022-10-25 00:59:45 +0200

    added send tx route

--- src/routes-tree/routes-index.ts
+++ src/routes-tree/routes-index.ts
@@ -3,10 +3,11 @@ const router = Express.Router();
 
 //routes import
 import mempool from "./routes/mempool";
+import sendTx from "./routes/send-tx";
 
 //using imports as routes
 router.use("/mempool", mempool);
-
+router.use("/send-transaction", sendTx);
 
 
 
