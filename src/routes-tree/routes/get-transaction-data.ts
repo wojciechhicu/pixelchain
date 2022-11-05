89c4bdafdcf6e77ba3c626436ad158c4816aa59f
Wojciech Marek <trustme@onet.pl>
2022-11-05 02:23:17 +0100

    commit

--- src/routes-tree/routes/get-transaction-data.ts
+++ src/routes-tree/routes/get-transaction-data.ts
@@ -8,38 +8,41 @@ const transactionData = Express.Router();
 transactionData.post('/', (req, res) => {
 	try{
 		let reqTx: TxHash = req.body;
-		let files = getFilesInDir().slice().reverse();
-		let txInFile: string = '';
-		files.forEach((val)=>{
-			let file: Index[] = JSON.parse(fs.readFileSync(`src/data/blockchain/indexes/${val}`, 'utf8'));
-			file.forEach((val)=>{
-				val.Tx?.forEach((value)=>{
-					if(value.txHash === reqTx.TxHash){
-						txInFile = val.blockInFile
-					}
-				})
-			})
-		})
-		if(txInFile.length == 0){
-			res.status(404).send(`No transaction in blockchain with hash ${reqTx}`)
-		} else {
-			fs.readFile(`src/data/blockchain/blocks/${txInFile}.json`,'utf8' ,(err, data:any)=>{
-				let blocks: Block[] = JSON.parse(data);
-				blocks.forEach((val)=>{
-					val.transactions.forEach((value)=>{
-						if(reqTx.TxHash === value.TxHash){
-							res.status(200).send(value)
-						}
-					})
-				})
-			})
 
-		}
+		// let files = getFilesInDir().slice().reverse();
+		// let txInFile: string = '';
+		// files.forEach((val)=>{
+		// 	let file: Index[] = JSON.parse(fs.readFileSync(`src/data/blockchain/indexes/${val}`, 'utf8'));
+		// 	file.forEach((val)=>{
+		// 		val.Tx?.forEach((value)=>{
+		// 			if(value.txHash === reqTx.TxHash){
+		// 				txInFile = val.blockInFile
+		// 			}
+		// 		})
+		// 	})
+		// })
+		// if(txInFile.length == 0){
+		// 	res.status(404).send(`No transaction in blockchain with hash ${reqTx}`)
+		// } else {
+		// 	fs.readFile(`src/data/blockchain/blocks/${txInFile}.json`,'utf8' ,(err, data:any)=>{
+		// 		let blocks: Block[] = JSON.parse(data);
+		// 		blocks.forEach((val)=>{
+		// 			val.transactions.forEach((value)=>{
+		// 				if(reqTx.TxHash === value.TxHash){
+		// 					res.status(200).send(value)
+		// 				}
+		// 			})
+		// 		})
+		// 	})
+
+		// }
 	} catch ( e ) {
 		res.send( e )
 	}
 });
-
+//FIXME przy dodawaniu transakcji dodawać do mem pool status 0 ( nie dodawać po prostu nie ma takiej to error) 1(to transakcje w mempool mają mieć) 2( jesli transakcja jest w blockchain dodawać opcję status)
+//TODO sprawdzac czy transakcja ma poprawne txhash
+//TODO przy dodawaniu transakcji sprawdzać czy można tyle wydać z tego portfela
 export = transactionData;
 
 interface TxHash {
