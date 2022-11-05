4e99f130c580e1aa12ee6c02b07330c293561180
Wojciech Marek <trustme@onet.pl>
2022-11-05 17:21:01 +0100

    repair status report

--- src/_helpers/blockchain/validator-validate-tx.ts
+++ src/_helpers/blockchain/validator-validate-tx.ts
@@ -3,6 +3,7 @@ import * as elliptic from 'elliptic';
 import { calcTxSHA256 } from '../../blockchain/transaction.functions'
 import { SHA256 } from "crypto-js";
 const ec = new elliptic.ec('secp256k1');
+import fs from 'fs';
 
 /**
  * Test is transaction valid to add to mempool
@@ -17,4 +18,34 @@ export function isReceivedTxValid(tx: InMempoolTransaction): boolean {
 
         const test = ec.keyFromPublic(tx.from, 'hex');
         return test.verify(calcTxSHA256(tx), tx.signature)
+}
+
+// export function walletHaveEnoughTokens(tx: InMempoolTransaction): boolean {
+//         if(tx.fee <= 0){
+//                 return false;
+//         }
+//         if(tx.txValue <= 0){
+//                 return false;
+//         }
+
+// }
+
+export function walletAlreadyInMempool(tx: InMempoolTransaction): boolean  {
+        try {
+                let rawFile: any = fs.readFileSync('src/data/mempool/transactions.json', 'utf8')
+                let file: InMempoolTransaction[] = JSON.parse(rawFile);
+                let check: number = 0;
+                file.forEach((val)=>{
+                        if(val.from == tx.from){
+                                check += 1;
+                        }
+                })
+                if(check > 0){
+                        return true
+                } else {
+                        return false
+                }
+        } catch( e ){
+                return false
+        }
 }
