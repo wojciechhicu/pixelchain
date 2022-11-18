55b86184186354ac8cccdb6a77bd11cbe1d384b2
Wojciech Marek <trustme@onet.pl>
2022-11-18 01:48:27 +0100

    refactor connect to network

--- src/modules/validator.ts
+++ src/modules/validator.ts
@@ -1,6 +1,6 @@
 import { seeLastBlockHeight } from './files.module';
 import { GenereteGenesisBlock } from './block.module';
-import { getConnectedNodes, isOtherNodesInNetwork } from './network.module';
+import { getConnectedNodes, isOtherNodesInNetwork, connectMeToNetwork } from './network.module';
 
 export function validator() {
         try{
@@ -11,9 +11,17 @@ export function validator() {
                         getConnectedNodes().then((peers)=>{
                                 if(peers != null){
                                         if(isOtherNodesInNetwork(peers)){
-                                                console.log("Other nodes detected.")
+                                                console.log("Other nodes detected.");
+                                                //TODO here goes multi node validation
                                         } else {
-                                                console.log("No other nodes detected.\nStarting network as single node.")
+                                                console.log("No other nodes detected.\nStarting network as single node.");
+                                                const connect = connectMeToNetwork()
+                                                if(connect){
+                                                        //TODO here goes validation as single node
+                                                } else {
+                                                        console.log("Cannot enter to network");
+                                                        throw 'Error'
+                                                }
                                         }
                                 } else {
                                         console.log("Cannot enter to network");
