403940fae4e475e0cc768742c64f615bbcc9134f
Wojciech Marek <trustme@onet.pl>
2022-11-30 02:59:24 +0100

    block time to 60 seconds

--- src/modules/validator.module.ts
+++ src/modules/validator.module.ts
@@ -153,7 +153,7 @@ export function singleNode(): void {
                                                                         }
                                                                 })
                                                         } else {
-                                                                console.log("Block is not valid, waiting...")
+                                                                console.log("Waiting for block time")
                                                         }
                                                 }
                                         })
